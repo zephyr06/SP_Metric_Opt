51d39a4a1632ca7c40343b8cf7684ff435dd7ab5
zephyr06 <sen.zephyr.wang@gmail.com>
2023-09-18 15:46:19 -0400

    add FiniteDist

--- sources/Safety_Performance_Metric/Probability.h
+++ sources/Safety_Performance_Metric/Probability.h
@@ -2,24 +2,18 @@
 #include <cmath>
 #include <vector>
 
+#include "sources/Utils/testMy.h"
 namespace SP_OPT_PA {
 
 class ProbabilityDistributionBase {
    public:
     ProbabilityDistributionBase() {}
 
-    double CDF(double x);
+    double CDF(double x) const;
 
     // virtual void UpdateDistribution(std::vector<double> execution_time_data);
 
     // data members
-};
-
-// stores a probability mass function, i.e., value-probability pair
-// this should be the major way to describe probability distribution
-class FiniteDist : public ProbabilityDistributionBase {
-    FiniteDist() {}
-    FiniteDist(double granularity) {}
 };
 
 class GaussianDist : public ProbabilityDistributionBase {
@@ -27,7 +21,7 @@ class GaussianDist : public ProbabilityDistributionBase {
     GaussianDist() {}
     GaussianDist(double mu, double sigma) : mu(mu), sigma(sigma) {}
 
-    inline double CDF(double x) {
+    inline double CDF(double x) const {
         double z = (x - mu) / (sigma * std::sqrt(2.0));
         return 0.5 * (1.0 + std::erf(z));
     }
@@ -37,4 +31,41 @@ class GaussianDist : public ProbabilityDistributionBase {
     double sigma;
 };
 
+struct Value_Proba {
+    Value_Proba() {}
+    Value_Proba(double v, double p) : value(v), probability(p) {}
+
+    double value;
+    double probability;
+};
+// stores a probability mass function, i.e., value-probability pair
+// this should be the major way to describe probability distribution
+class FiniteDist : public ProbabilityDistributionBase {
+   public:
+    FiniteDist() {}
+    FiniteDist(const GaussianDist& gauss_dist, double min_val, double max_val,
+               double granularity) {
+        distribution.reserve(granularity);
+        if (granularity < 1)
+            CoutError("Invalid granularity!");
+
+        double step = (max_val - min_val) / (granularity - 1);
+        distribution.push_back(Value_Proba(min_val, gauss_dist.CDF(min_val)));
+
+        for (double execution_time = min_val + step; execution_time <= max_val;
+             execution_time += step) {
+            double chance = gauss_dist.CDF(execution_time) -
+                            gauss_dist.CDF(execution_time - step);
+            distribution.push_back(Value_Proba(execution_time, chance));
+        }
+    }
+
+    inline size_t size() const { return distribution.size(); }
+
+    inline Value_Proba& operator[](size_t i) { return distribution[i]; }
+    inline const Value_Proba& at(size_t i) { return distribution.at(i); }
+    // data
+    // saves the probability that x<= value
+    std::vector<Value_Proba> distribution;
+};
 }  // namespace SP_OPT_PA
--- tests/testProbability.cpp
+++ tests/testProbability.cpp
@@ -13,7 +13,16 @@ TEST(CDF, v1) {
     GaussianDist gau_dis(3, 1);
     EXPECT_EQ(0.5, gau_dis.CDF(3));
 }
+TEST(FiniteDist, V1) {
+    GaussianDist gau_dis(10, 1);
+    FiniteDist finite_dis(gau_dis, 5, 15, 11);
+    EXPECT_EQ(11, finite_dis.size());
+    EXPECT_EQ(5, finite_dis[0].value);
+    EXPECT_NEAR(2.866e-7, finite_dis[0].probability, 1e-8);
 
+    EXPECT_EQ(6, finite_dis[1].value);
+    EXPECT_NEAR(3.167e-5 - 2.866e-7, finite_dis[1].probability, 1e-6);
+}
 int main(int argc, char **argv) {
     // ::testing::InitGoogleTest(&argc, argv);
     ::testing::InitGoogleMock(&argc, argv);
