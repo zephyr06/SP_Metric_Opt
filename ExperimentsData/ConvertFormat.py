11fee64867e3e34ebc8f2d68c56eef354788c0d8
zephyr06 <sen.zephyr.wang@gmail.com>
2023-09-19 18:12:18 -0400

    update figure plots

--- ExperimentsData/ConvertFormat.py
+++ ExperimentsData/ConvertFormat.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import numpy as np
 import seaborn as sns
 
@@ -8,6 +9,17 @@ def is_float(s):
         return True
     except ValueError:
         return False
+
+def gaussian(x, amplitude, mean, std_dev):
+    return amplitude * np.exp(-((x - mean) ** 2) / (2 * std_dev ** 2))
+
+def gauss_fit(data):
+    # initial_guess = [1.0, np.mean(data), np.std(data)]
+    # params, covariance = curve_fit(gaussian, np.arange(len(data)), data, p0=initial_guess)
+    # amplitude, mean, std_dev = params
+    print("Mean: ", np.mean(data), "Variance: ", np.std(data))
+
+
 
 def read_data(file_path, data_index):
     data_all=[]
@@ -19,7 +31,8 @@ def read_data(file_path, data_index):
                 data_elements = line.split(" ")
                 if(is_float(data_elements[data_index])):
                     data_all.append(float(data_elements[data_index]))
-                    print(float(data_elements[data_index]))
+                    # print(float(data_elements[data_index]))
+            gauss_fit(data_all)
             return data_all
 
     except FileNotFoundError:
@@ -33,12 +46,12 @@ def write_data(data, file_name):
     for et in data:
         file.write(str(et)+"\n")
 
-    sns.kdeplot(data, shade=True)
-    plt.xlim(min(data), max(data))
+    # sns.kdeplot(data, shade=True)
+    # plt.xlim(min(data), max(data))
     print("Limits: ",min(data), max(data))
     plt.xlabel(file_name)
     plt.savefig(file_name+".png")
-    plt.show()
+    # plt.show()
 
 def main():
     data = read_data("MPC_time.txt", 1)
