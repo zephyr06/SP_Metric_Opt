bc658a5e988830421c8407aa4b5841379699c347
zephyr06 <sen.zephyr.wang@gmail.com>
2024-03-27 19:48:52 -0400

    add unit tests for draw_SP

--- Visualize_SP_Metric/draw_SP.py
+++ Visualize_SP_Metric/draw_SP.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import os
 import yaml
 
-
+PROJECT_PATH = "/home/zephyr/Programming/SP_Metric_Opt"
+# All time in seconds
 
 def verify_task_set_config(path):
     if os.path.exists(path):
@@ -11,18 +12,24 @@ def verify_task_set_config(path):
     else:
         print(path, " does not exist.")
 
+
 def get_app2period(task_set_config):
     app_name2period = {}
     with open(task_set_config, 'r') as file:
         yaml_data = yaml.safe_load(file)
-        for i in range(len(yaml_data)):
+        for i in range(len(yaml_data['tasks'])):
             data_entry = yaml_data['tasks'][i]
+            app_name2period[data_entry['name']] = data_entry['period']/1e3
     return app_name2period
+
 
 def get_publisher_file_path(folder_path, task_name):
     return os.path.join(folder_path, "publisher_" + task_name.lower() + ".txt")
+
+
 def get_subscription_file_path(folder_path, task_name):
     return os.path.join(folder_path, task_name.lower() + "_subscriber.txt")
+
 
 def get_index_to_data_map(file_path):
     verify_task_set_config(file_path)
@@ -44,47 +51,91 @@ class TaskInfo:
     def __init__(self, name, period):
         self.name = name
         self.period = period
-        self.publisher_index2_data = {}
-        self.publisher_offset=0
-        self.subscriber_index2_data = {}
-        self.subscriber_offset=0
-        
+        self.publisher_index2data = {}
+        self.publisher_offset = 0
+        self.subscriber_index2data = {}
+        self.subscriber_offset = 0
+        self.skipped_instance_response_time = 1e9
+        self.response_time_index2data={}
+
     def load_publish_data(self, data_folder_path):
         file_path = get_publisher_file_path(data_folder_path, self.name)
-        self.publisher_offset, self.publisher_index2_data = get_index_to_data_map(file_path)
+        self.publisher_offset, self.publisher_index2data = get_index_to_data_map(
+            file_path)
 
     def load_subscribe_data(self, data_folder_path):
         file_path = get_subscription_file_path(data_folder_path, self.name)
-        self.subscriber_offset, self.subscriber_index2_data = get_index_to_data_map(file_path)
+        self.subscriber_offset, self.subscriber_index2data = get_index_to_data_map(
+            file_path)
+
+    def get_response_time_index2data(self):
+        if len(self.response_time_index2data) == 0:
+            for index in self.publisher_index2data:
+                if self.subscriber_index2data.get(index) is None:
+                    self.response_time_index2data[index] = self.skipped_instance_response_time
+                else:
+                    self.response_time_index2data[index] = self.subscriber_index2data[index] + \
+                        self.subscriber_offset - \
+                        self.publisher_index2data[index] - self.publisher_offset
+        return self.response_time_index2data
+    
+    def publisher_index2actual_time(self, index):
+        return self.publisher_index2data[index] * self.period + self.publisher_offset
+    
+    def get_response_time_within_range(self, start_time, end_time):
+        response_time_index2data = self.get_response_time_index2data()
+        response_time_within_range = []
+        for index in self.publisher_index2data:
+            release_time = self.publisher_index2actual_time(index)
+            if release_time >= start_time and release_time < end_time:
+                response_time_within_range.append(response_time_index2data[index]) 
+        return response_time_within_range
+
+
+def normalize_offsets(tasks_name_to_info):
+    start_time = []
+    for name, task_info in tasks_name_to_info.items():
+        start_time.append(task_info.publisher_offset)
+    min_start_time = min(start_time)
+
+    for task in tasks_name_to_info:
+        tasks_name_to_info[task].publisher_offset -= min_start_time
+        tasks_name_to_info[task].subscriber_offset -= min_start_time
+    return tasks_name_to_info
+
+def get_task_set_info(tasks_name_list, app_name2period, data_folder_path):
+    tasks_name_to_info = {}
+    for task in tasks_name_list:
+        task_info = TaskInfo(task, app_name2period[task])
+        tasks_name_to_info[task] = task_info
+        tasks_name_to_info[task].load_publish_data(data_folder_path)
+        tasks_name_to_info[task].load_subscribe_data(data_folder_path)
+    return normalize_offsets(tasks_name_to_info)
+
 
 
 if __name__ == "__main__":
-    PROJECT_PATH = "/home/zephyr/Programming/SP_Metric_Opt"
-    data_folder_path = os.path.join(PROJECT_PATH, "Visualize_SP_Metric","data")
-    task_set_config = os.path.join(PROJECT_PATH, "TaskData/test_robotics_v4.yaml") 
+    data_folder_path = os.path.join(
+        PROJECT_PATH, "Visualize_SP_Metric", "data")
+    task_set_config = os.path.join(
+        PROJECT_PATH, "TaskData/test_robotics_v4.yaml")
     verify_task_set_config(task_set_config)
     app_name2period = get_app2period(task_set_config)
-    tasks_name_list =['TSP','RRT','SLAM','MPC']
+    tasks_name_list = ['TSP', 'RRT', 'SLAM', 'MPC']
 
-    horizon_granularity = 10000 # 10 seconds
-    horizon = 1e5 # 100 seconds
-    discard_early_time = 2e4 # 20 seconds
+    horizon_granularity = 10  # 10 seconds
+    horizon = 100  # 100 seconds
+    discard_early_time = 20  # 20 seconds
 
     # offset, index_to_data = get_index_to_data_map(get_publisher_file_path(data_folder_path, 'MPC'))
-    offset, index_to_data = get_index_to_data_map(get_subscription_file_path(data_folder_path, 'SLAM'))
-    tasks_name_to_info={}
+    offset, index_to_data = get_index_to_data_map(
+        get_subscription_file_path(data_folder_path, 'SLAM'))
+    tasks_name_to_info = get_task_set_info(tasks_name_list, app_name2period, data_folder_path)
+
     for task in tasks_name_list:
-        tasks_name_to_info[task] = TaskInfo(task, app_name2period[task])
-        tasks_name_to_info[task].load_publish_data(data_folder_path)
-        tasks_name_to_info[task].load_subscribe_data(data_folder_path)
-    
-    print(offset)
-    print(index_to_data)
+        print(task, tasks_name_to_info[task].publisher_offset,
+              tasks_name_to_info[task].subscriber_offset)
 
-
-
-
-
-
-
-
+    for start_time in range(discard_early_time, horizon, horizon_granularity):
+        end_time = start_time + horizon_granularity
+        # for task_name in tasks_name_list:
---
+++ Visualize_SP_Metric/test_TaskInfo.py
@@ -0,0 +1,59 @@
+import pytest
+
+from draw_SP import *
+
+def test_read_period():
+    task_set_config = os.path.join(
+        PROJECT_PATH, "TaskData/test_robotics_v4.yaml")
+    verify_task_set_config(task_set_config)
+    app_name2period = get_app2period(task_set_config)
+    assert app_name2period['TSP'] == 1.0
+    assert app_name2period['MPC'] == 0.5
+    assert app_name2period['RRT'] == 1.0
+    assert app_name2period['SLAM'] == 1.0
+
+def test_load_publish_data():
+    task = TaskInfo('MPC', 1.0)
+    data_folder_path = os.path.join(
+        PROJECT_PATH, "Visualize_SP_Metric", "data_for_test")
+    task.load_publish_data(data_folder_path)
+    assert task.publisher_offset == pytest.approx(1711401393.7642669678)
+    assert task.publisher_index2data[0] == pytest.approx(0.61328400)
+
+    task.load_subscribe_data(data_folder_path)
+    assert task.subscriber_offset == pytest.approx(1711401393.764341116)
+    assert task.subscriber_index2data[0] == pytest.approx(0.750322999)
+
+def test_get_response_time():
+    task = TaskInfo('MPC', 1.0)
+    data_folder_path = os.path.join(
+        PROJECT_PATH, "Visualize_SP_Metric", "data_for_test")
+    task.load_publish_data(data_folder_path)
+    task.load_subscribe_data(data_folder_path)
+    response_time_index2data = task.get_response_time_index2data()
+    assert response_time_index2data[0] == pytest.approx(0.13703999999999998, abs=1e-3)
+    assert response_time_index2data[100] == pytest.approx(50.7210350 - 50.614, abs=1e-3)
+
+def test_publisher_index2actual_time():
+    task = TaskInfo('MPC', 1.0)
+    data_folder_path = os.path.join(
+        PROJECT_PATH, "Visualize_SP_Metric", "data_for_test")
+    task.load_publish_data(data_folder_path)
+    assert task.publisher_index2actual_time(0) == pytest.approx(1711401393.7642669678)
+    assert task.publisher_index2actual_time(100) == pytest.approx(1711401393.7642669678 + 100 * 1.0)
+
+def test_normalize_offsets():
+    tasks_name_list = ['TSP', 'RRT', 'SLAM', 'MPC']
+
+    task_set_config = os.path.join(
+        PROJECT_PATH, "TaskData/test_robotics_v4.yaml")
+    app_name2period = get_app2period(task_set_config)
+
+    data_folder_path = os.path.join(
+        PROJECT_PATH, "Visualize_SP_Metric", "data_for_test")
+    
+    tasks_name_to_info = get_task_set_info(tasks_name_list, app_name2period, data_folder_path)
+    assert tasks_name_to_info['SLAM'].publisher_offset == pytest.approx(0.0, abs=1e-4)
+    assert tasks_name_to_info['SLAM'].subscriber_offset == pytest.approx(12, abs=1e0)
+    assert tasks_name_to_info['MPC'].publisher_offset == pytest.approx(0.0, abs=1e-2)
+    assert tasks_name_to_info['MPC'].subscriber_offset == pytest.approx(0.0, abs=1e-2)
